ab7f3fb06138f76cd7a23b51e94ab1c3b3ee5db4
jamesexantus <jamesexantus@ymail.com>
2023-06-22 14:50:35 -0400

    second

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,14 +1,17 @@
 function addToCart(item, quantity){
-
+    return item * quantity;
 }
 
 function removeFromCart(item, quantity){
-
-
+    return item - quantity;
 }
 
 function calculateTotal(item, quantity) {
-
-
+   console.log(item, quantity);
 }
 
+module.exports = {
+    addToCart : addToCart,
+    removeFromCart : removeFromCart,
+    calculateTotal : calculateTotal,
+}
---
+++ shoppingCart.test.js
@@ -0,0 +1,9 @@
+const shoppingCart = require('./shoppingCart');
+
+
+/expected pass
+describe("addToCart(apple, 4) returns ", () => {
+    test("reverseString(car) => rac", () => {
+        expect(stringFunctions.reverseString("car")).toEqual("rac");
+    })
+})  
